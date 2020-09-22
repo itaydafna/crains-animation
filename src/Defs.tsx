2ffef8be15caa2cdf5236adbdada6fd6dcd6e888
Itay Dafna <idafna@salesforce.com>
2020-09-22 21:16:11 +0300

    render icono

---
+++ src/Defs.tsx
@@ -0,0 +1,190 @@
+import React from 'react';
+
+const Defs = () => <defs>
+  <linearGradient
+    id="linear-gradient"
+    x1="-30.87"
+    y1="595.37"
+    x2="-29.73"
+    y2="594.91"
+    gradientTransform="matrix(0, 2.56, 2.56, 0, -1212.18, 256.46)"
+    gradientUnits="userSpaceOnUse"
+  >
+    <stop offset={0} stopColor="#4d75f2" />
+    <stop offset={1} stopColor="#7896f3" />
+  </linearGradient>
+  <linearGradient
+    id="linear-gradient-2"
+    x1="-30.88"
+    y1="595.37"
+    x2="-29.74"
+    y2="594.91"
+    gradientTransform="matrix(0, 2.56, 2.56, 0, -1212.18, 262.8)"
+    xlinkHref="#linear-gradient"
+  />
+  <linearGradient
+    id="linear-gradient-3"
+    x1="-102.57"
+    y1="558.36"
+    x2="-101.43"
+    y2="557.9"
+    gradientTransform="matrix(0, 10.05, 10.05, 0, -5335.68, 1181.49)"
+    xlinkHref="#linear-gradient"
+  />
+  <linearGradient
+    id="linear-gradient-4"
+    x1="-78.08"
+    y1={571}
+    x2="-76.95"
+    y2="570.54"
+    gradientTransform="matrix(0, 5.02, 5.02, 0, -2609.98, 575.64)"
+    xlinkHref="#linear-gradient"
+  />
+  <linearGradient
+    id="linear-gradient-5"
+    x1="-78.08"
+    y1={571}
+    x2="-76.95"
+    y2="570.54"
+    gradientTransform="matrix(0, 5.02, 5.02, 0, -2630.73, 550.84)"
+    xlinkHref="#linear-gradient"
+  />
+  <filter
+    id="filter-2"
+    x="-10.4%"
+    y="-12.1%"
+    width="120.8%"
+    height="124.2%"
+    name="filter-2"
+  >
+    <feOffset result="shadowOffsetOuter1" in="SourceAlpha" />
+    <feGaussianBlur
+      result="shadowBlurOuter1"
+      stdDeviation={2}
+      in="shadowOffsetOuter1"
+    />
+    <feColorMatrix
+      values="0 0 0 0 0.03 0 0 0 0 0.11 0 0 0 0 0.33 0 0 0 0.25 0"
+      in="shadowBlurOuter1"
+    />
+  </filter>
+  <mask
+    id="mask"
+    x="197.7"
+    y="141.27"
+    width="27.1"
+    height="20.66"
+    maskUnits="userSpaceOnUse"
+  >
+    <g id="mask-4">
+      <path
+        id="path-3"
+        d="M224.8,141.27v20.65H206.72q.81-8.34,0-10.33c-.49-1.23-2.76-.67-3.81-.67H197.7l9-9.65Z"
+        fill="#fff"
+        fillRule="evenodd"
+      />
+    </g>
+  </mask>
+  <filter
+    id="filter-5"
+    x="-15.9%"
+    y="-15%"
+    width="131.9%"
+    height="130%"
+    name="filter-5"
+  >
+    <feGaussianBlur stdDeviation="0.6" in="SourceGraphic" />
+  </filter>
+  <linearGradient
+    id="linear-gradient-6"
+    x1="-52.99"
+    y1="583.96"
+    x2="-51.85"
+    y2="583.5"
+    gradientTransform="matrix(0, 3.32, 3.32, 0, -1718.29, 335.34)"
+    xlinkHref="#linear-gradient"
+  />
+  <linearGradient
+    id="linear-gradient-7"
+    x1="-176.7"
+    y1="449.55"
+    x2="-175.56"
+    y2="449.09"
+    gradientTransform="translate(-386.04 1190.4) rotate(180) scale(2.56 2.56)"
+    xlinkHref="#linear-gradient"
+  />
+  <linearGradient
+    id="linear-gradient-8"
+    x1="-176.7"
+    y1="449.55"
+    x2="-175.56"
+    y2="449.09"
+    gradientTransform="translate(-392.38 1190.4) rotate(180) scale(2.56 2.56)"
+    xlinkHref="#linear-gradient"
+  />
+  <linearGradient
+    id="linear-gradient-9"
+    x1="-139.69"
+    y1="521.24"
+    x2="-138.55"
+    y2="520.78"
+    gradientTransform="translate(-1311.07 5313.9) rotate(180) scale(10.05 10.05)"
+    xlinkHref="#linear-gradient"
+  />
+  <linearGradient
+    id="linear-gradient-10"
+    x1="-152.33"
+    y1="496.76"
+    x2="-151.19"
+    y2="496.3"
+    gradientTransform="translate(-702.22 2601.2) rotate(180) scale(5.02)"
+    xlinkHref="#linear-gradient"
+  />
+  <linearGradient
+    id="linear-gradient-11"
+    x1="-152.33"
+    y1="496.76"
+    x2="-151.19"
+    y2="496.3"
+    gradientTransform="translate(-677.42 2621.95) rotate(180) scale(5.02 5.02)"
+    xlinkHref="#linear-gradient"
+  />
+  <mask
+    id="mask-2"
+    x="75.56"
+    y="139.06"
+    width="28.04"
+    height="28.06"
+    maskUnits="userSpaceOnUse"
+  >
+    <g id="mask-11">
+      <path
+        id="path-10"
+        d="M103.6,150.31,86.27,139.06l-9.84,15.16q7.42,3.85,8.66,5.62c.76,1.09,2.84.76,3.16,1.74q.3.93,1.93,5.54l3.57-1.65Z"
+        fill="#fff"
+        fillRule="evenodd"
+      />
+    </g>
+  </mask>
+  <filter
+    id="filter-12"
+    x="-16.1%"
+    y="-27.2%"
+    width="132.2%"
+    height="154.4%"
+    name="filter-12"
+  >
+    <feGaussianBlur stdDeviation="0.6" in="SourceGraphic" />
+  </filter>
+  <linearGradient
+    id="linear-gradient-12"
+    x1="-165.28"
+    y1="471.66"
+    x2="-164.14"
+    y2="471.2"
+    gradientTransform="translate(-461.92 1709.52) rotate(180) scale(3.32 3.32)"
+    xlinkHref="#linear-gradient"
+  />
+</defs>;
+
+export default Defs;
